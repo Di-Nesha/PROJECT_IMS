09cb474657588a24f18f2a1d946bf41a902a5888
Dinesha Bandara <dineshalasanthi@gmail.com>
2022-04-02 13:37:07 +0530

    Employee_POST

--- employee/views.py
+++ employee/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import EmployeeForm
 
 #Create your views here.
@@ -8,8 +8,14 @@ def employee_list(request):
 
 # Insert & Update Function
 def employee_form(request):
-        form = EmployeeForm()
-        return render(request,"employee/employee_form.html",{'form':form})
+        if request.method == "GET":
+                form = EmployeeForm()
+                return render(request,"employee/employee_form.html",{'form':form})
+        else:
+                form = EmployeeForm(request.POST)
+                if form.is_valid():
+                        form.save()
+                return redirect('/employee/list')
 
 #Delete Function
 def employee_delete(request):
