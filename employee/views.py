18087b29bffb3a5e9e2b38f45dec2b6108861d0d
Dinesha Bandara <dineshalasanthi@gmail.com>
2022-04-03 14:04:43 +0530

    Employee_Update

--- employee/views.py
+++ employee/views.py
@@ -8,12 +8,20 @@ def employee_list(request):
         return render(request,"employee/employee_list.html",context)
 
 # Insert & Update Function
-def employee_form(request):
+def employee_form(request, id=0):
         if request.method == "GET":
-                form = EmployeeForm()
+                if id==0:
+                        form = EmployeeForm()
+                else:
+                        employee = New.objects.get(pk=id)
+                        form = EmployeeForm(instance=employee)
                 return render(request,"employee/employee_form.html",{'form':form})
         else:
-                form = EmployeeForm(request.POST)
+                if id==0:
+                        form = EmployeeForm(request.POST)
+                else:
+                        employee = New.objects.get(pk=id)
+                        form = EmployeeForm(request.POST,instance=employee)
                 if form.is_valid():
                         form.save()
                 return redirect('/employee/list')
