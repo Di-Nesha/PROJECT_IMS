6f515b2c7626beae5247056c196213db98d8f02b
Dinesha Bandara <dineshalasanthi@gmail.com>
2022-04-03 14:04:39 +0530

    Employee_Update

--- employee/urls.py
+++ employee/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.employee_form), #localhost:portnumber/employee
-    path('list/',views.employee_list)
+    path('',views.employee_form, name='employee_insert'), # GET & POST request for INSERT operation
+    path('<int:id>/',views.employee_form, name='employee_update'), # GET & POST request for UPDATE operation
+    path('list/',views.employee_list, name='employee_list') # GET request to retrive and display all records
 ]
