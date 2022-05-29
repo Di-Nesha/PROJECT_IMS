ee59c3b701f603d1b332fdb00d62a2cf245a9df8
Dinesha Bandara <dineshalasanthi@gmail.com>
2022-05-29 18:00:45 +0530

    Final_inventory_V1

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.inventory_form, name='inventory_insert'), # GET & POST request for INSERT operation
     path('<int:id>/',views.inventory_form, name='inventory_update'), # GET & POST request for UPDATE operation
     path('delete/<int:id>/',views.inventory_delete, name='inventory_delete'), # GET & POST request for DELETE operation
-    path('list/',views.inventory_list, name='inventory_list') # GET request to retrive and display all records
+    path('list/',views.inventory_list, name='inventory_list'), # GET request to retrive and display all records
+    path('inventory_pdf', views.inventory_pdf, name='inventory_pdf'),
+
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -2,6 +2,40 @@ from django.shortcuts import render,redirect
 from .forms import InventoryForm
 from .models import Inventory
 
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+#Create your PDF here.
+def inventory_pdf(request):
+        buf = io.BytesIO()
+        c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+        textob = c.beginText()
+        textob.setTextOrigin(inch,inch)
+        textob.setFont("Courier", 14)
+
+        inventorys = Inventory.objects.all()
+
+        lines = []
+
+        for inventory in inventorys:
+                # lines.append(inventory.availableqty)
+                lines.append(inventory.status.name)
+                lines.append(inventory.employee.callingname)
+                lines.append("-------------------------------------------")
+
+        for line in lines:
+                textob.textLine(line)
+
+        c.drawText(textob)
+        c.showPage()
+        c.save()
+        buf.seek(0)
+
+        return FileResponse(buf, as_attachment=True, filename="Inventory.pdf")
+
 #Create your views here.
 def inventory_list(request):
         context = {'inventory_list':Inventory.objects.all()}
