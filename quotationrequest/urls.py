6f9b69383cf9b332af19f226d54b687ae7b30a66
Dinesha Bandara <dineshalasanthi@gmail.com>
2022-05-29 17:39:59 +0530

    Final_BidRequest_V1

--- quotationrequest/urls.py
+++ quotationrequest/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('',views.quotationrequest_form, name='quotationrequest_insert'), # GET & POST request for INSERT operation
     path('<int:id>/',views.quotationrequest_form, name='quotationrequest_update'), # GET & POST request for UPDATE operation
     path('delete/<int:id>/',views.quotationrequest_delete, name='quotationrequest_delete'), # GET & POST request for DELETE operation
-    path('list/',views.quotationrequest_list, name='quotationrequest_list') # GET request to retrive and display all records
+    path('list/',views.quotationrequest_list, name='quotationrequest_list'), # GET request to retrive and display all records
+    path('bidrequest_pdf', views.bidrequest_pdf, name='bidrequest_pdf'),
+
 ]
--- quotationrequest/views.py
+++ quotationrequest/views.py
@@ -2,6 +2,41 @@ from django.shortcuts import render,redirect
 from .forms import QuotationRequestForm
 from .models import QuotationRequest
 
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+#Create your PDF here.
+def bidrequest_pdf(request):
+        buf = io.BytesIO()
+        c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+        textob = c.beginText()
+        textob.setTextOrigin(inch,inch)
+        textob.setFont("Courier", 14)
+
+        bidrequests = QuotationRequest.objects.all()
+
+        lines = []
+
+        for bidrequest in bidrequests:
+                lines.append(bidrequest.number)
+                lines.append(bidrequest.supplier.name)
+                lines.append(bidrequest.status.name)
+                lines.append(bidrequest.employee.callingname)
+                lines.append("-------------------------------------------")
+
+        for line in lines:
+                textob.textLine(line)
+
+        c.drawText(textob)
+        c.showPage()
+        c.save()
+        buf.seek(0)
+
+        return FileResponse(buf, as_attachment=True, filename="Bid-Request.pdf")
+
 #Create your views here.
 def quotationrequest_list(request):
         context = {'quotationrequest_list':QuotationRequest.objects.all()}
