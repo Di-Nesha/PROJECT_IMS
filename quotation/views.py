4b1a2c0a591e6e4be0e3ee57840722915be14966
Dinesha Bandara <dineshalasanthi@gmail.com>
2022-05-29 17:49:01 +0530

    Final_Quotation_V1

--- quotation/views.py
+++ quotation/views.py
@@ -2,6 +2,41 @@ from django.shortcuts import render,redirect
 from .forms import QuotationForm
 from .models import Quotation
 
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+#Create your PDF here.
+def quotation_pdf(request):
+        buf = io.BytesIO()
+        c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+        textob = c.beginText()
+        textob.setTextOrigin(inch,inch)
+        textob.setFont("Courier", 14)
+
+        quotations = Quotation.objects.all()
+
+        lines = []
+
+        for quotation in quotations:
+                lines.append(quotation.number)
+                lines.append(quotation.status.name)
+                lines.append(quotation.quotationrequest.number)
+                lines.append(quotation.employee.callingname)
+                lines.append("-------------------------------------------")
+
+        for line in lines:
+                textob.textLine(line)
+
+        c.drawText(textob)
+        c.showPage()
+        c.save()
+        buf.seek(0)
+
+        return FileResponse(buf, as_attachment=True, filename="Quotation.pdf")
+
 #Create your views here.
 def quotation_list(request):
         context = {'quotation_list':Quotation.objects.all()}
