df7ab3963cc7197805e8475230c4abb8abacd004
Dinesha Bandara <dineshalasanthi@gmail.com>
2022-06-05 20:04:22 +0530

    Register_not_working

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+app_name = "core" 
+
 urlpatterns = [
  path('admin/', admin.site.urls),
  path('', views.Index.as_view(), name='index'),
  path('login/', views.Login.as_view(), name='login'),
- path('register/', views.Login.as_view(), name='register'),
- path('401.html', views.Login.as_view(), name='401'),
+path('401.html', views.Login.as_view(), name='401'),
  path('404/', views.Login.as_view(), name='404'),
  path('500/', views.Login.as_view(), name='500'),
 
--- core/views.py
+++ core/views.py
@@ -2,22 +2,34 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from employee.models import New
+from .forms import NewUserForm
+from django.contrib import messages
 
+class Register(View):
+    template = 'register.html'
+
+    def register_request(request):
+        if request.method == "POST":
+            form = NewUserForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                login(request, user)
+                messages.success(request, "Registration successful." )
+                return redirect("login")
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+        form = NewUserForm()
+        return render (request=request, template_name="register", context={"form":form})
+
+    def get(self, request):
+        form = NewUserForm()
+        return render(request, self.template, {'form': form})
 
 class Password(LoginRequiredMixin, View):
     template = 'password.html'
     login_url = '/password/'
- 
-    def get(self, request):
-        return render(request, self.template)
-
-
-class Register(LoginRequiredMixin, View):
-    template = 'register.html'
-    login_url = '/register/'
  
     def get(self, request):
         return render(request, self.template)
@@ -51,3 +63,4 @@ class Login(View):
             return HttpResponseRedirect('/')
         else:
             return render(request, self.template, {'form': form})
+
